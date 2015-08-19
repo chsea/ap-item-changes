49c4596e9b22eb640e978d7d7449a052350c1bae
Chelsea Du <chsea.du@gmail.com>
2015-08-18 23:59:06 -0400

    add models

--- app.js
+++ app.js
@@ -3,29 +3,44 @@ var Promise = require('bluebird');
 var readFile = Promise.promisify(require("fs").readFile);
 var https = require("https");
 var models = require('./index.js');
-var Match = models.Match;
+var Participant = models.Participant;
 
-// var matchPath = './AP_ITEM_DATASET/5.11/NORMAL_5X5/';
-// var matches = readFile(matchPath + 'NA.json').then(function(matches) {
-//   return JSON.parse(matches).slice(0, 10);
-// });
-//
-// matches.then(function(matches) {
-//   matches.forEach
-// })
+var matchPath = './AP_ITEM_DATASET/5.11/NORMAL_5X5/';
+var matches = readFile(matchPath + 'NA.json').then(function(matches) {
+  return JSON.parse(matches).slice(1, 2);
+});
 
-https.get('https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/seaasong?api_key=daf26bdd-8fb1-4722-b26c-496eed56edbc', function(res) {
-	var responseBody = "";
+matches.then(function(matches) {
+  matches.forEach(function(match) {
+    https.get('https://na.api.pvp.net/api/lol/na/v2.2/match/' + match + '?includeTimeline=true&api_key=daf26bdd-8fb1-4722-b26c-496eed56edbc', function(res) {
+    	var matchData = "";
+    	res.on('data', function(dataChunk) {
+    		matchData += dataChunk;
+      });
 
-	res.on('data', function(dataChunk) {
-		responseBody += dataChunk;
+    	res.on('end', function(){
+    		matchData = JSON.parse(matchData);
+        matchData.participants.forEach(function(p) {
+          var stats = p.stats;
+          var participant = {
+    				champion: p.championId,
+    				lane: p.timeline.lane,
+    				items: [stats.item0, stats.item1, stats.item2, stats.item3, stats.item4, stats.item5, stats.item6],
+    				winner: stats.winner,
+    				damageDealt: stats.totalDamageDealt,
+    				kills: stats.kills,
+    				deaths: stats.deaths,
+    				assists: stats.assists,
+    				postPatch: false
+          };
+          Participant.create(participant, function(err, data) {
+      			if(err) console.error(err);
+            console.log("created: ", data);
+          });
+        });
+    	});
+    }).on('error', function(err) {
+      	console.error(err);
+    });
   });
-
-	res.on('end', function(){
-		responseBody = JSON.parse(responseBody);
-		console.log("data: ", responseBody);
-	});
-
-}).on('error', function(err) {
-  	console.error(err);
 });
--- index.js
+++ index.js
@@ -3,13 +3,48 @@ mongoose.connect('mongodb://localhost/league');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'mongodb connection error:'));
 
-var matchSchema = new mongoose.Schema({
-  id: {type: Number, required: true },
-  champions: {type: [Number], required: true}
+var participantSchema = new mongoose.Schema({
+  champion: {type: Number, required: true},
+  lane: {type: String, required: true},
+  items: {type: [Number], required: true},
+  winner: {type: Boolean, required: true},
+  damageDealt: {type: Number, required: true},
+  kills: {type: Number, required: true},
+  deaths: {type: Number, required: true},
+  assists: {type: Number, required: true},
+  postPatch: {type: Boolean, required: true}
 });
 
-var Match = mongoose.model('Match', matchSchema);
+participantSchema.virtual('kda').get(function(){
+  return (this.kills + this.assists) / this.deaths;
+});
+
+participantSchema.methods.hasItem = function(item) {
+  return this.items.some(function(currentItem) {
+    return currentItem === item;
+  });
+};
+
+var itemSchema = new mongoose.Schema({
+  id: {type: Number, required: true},
+  name: {type: String, required: true},
+  countPre: {type: Number},
+  countPost: {type: Number}
+});
+
+var championSchema = new mongoose.Schema({
+  id: {type: Number, required: true},
+  name: {type: String, required: true},
+  countPre: {type: Number},
+  countPost: {type: Number}
+});
+
+var Participant = mongoose.model('Participant', participantSchema);
+var Item = mongoose.model('Item', itemSchema);
+var Champion = mongoose.model('Champion', championSchema);
 
 module.exports = {
-  Match: Match
+  Participant: Participant,
+  Item: Item,
+  Champion: Champion
 };
